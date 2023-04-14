df503165f2338a5f84f6a6e546411c2cebef763e
nucasspro <hailongdang.hailong@gmail.com>
2023-04-14 13:05:57 +0700

    Add CORS

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+buidler.Services.AddCors(options =>
+     {
+         options.AddPolicy("NewPolicy", builder =>
+          builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader());
+     });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+
+var app = builder.Build();
+app.UseCors("CorsAllowAll");
+await app.RunAsync();
