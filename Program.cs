127b9f8dab7f6c96d67656541420699583f45c9c
shawrylk <33708806+shawrylk@users.noreply.github.com>
2023-04-14 13:19:06 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,17 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
-buidler.Services.AddCors(options =>
-     {
-         options.AddPolicy("NewPolicy", builder =>
-          builder.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader());
-     });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
-var app = builder.Build();
-app.UseCors("CorsAllowAll");
-await app.RunAsync();
+await builder.Build().RunAsync();
